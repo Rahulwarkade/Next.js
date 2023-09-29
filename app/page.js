c5d6f31dadbf63408ca86c50431868ee27e1ceb5
Rahul Warkade <rahulwarkade954@gmail.com>
2023-09-29 20:45:02 +0530

    Nextjs

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 "use client"
 import React, { Fragment, useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 const page = () => {
-  const [value, setvalue] = useState(0)
-
-  const change = ()=>{
-    setvalue(24)
-    console.log(value);
-  }
+  
   return (
    <Fragment>
-    <h1>This is next-react {value}.</h1>
-    <button onClick={change}>Change</button>
+    <div className="container">
+      <input type="text" className = "form-control mt-5" placeholder= "Title" />
+      <textarea name="task" id="task" placeholder = "discription here ....." className='form-control mt-3 '></textarea>
+      <select className="form-control  mt-3" >
+        <option value="Due">Due</option>
+        <option value="Pending">Pending</option>
+        <option value="Completed">Completed</option>
+      </select>
+      <button type="submit" className='btn btn-primary mt-3'>Submit</button>
+    </div>
    </Fragment>
   )
 }
