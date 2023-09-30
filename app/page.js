d8d5561762a12fdf2c1233d97603dd0d572e357e
Rahul Warkade <rahulwarkade954@gmail.com>
2023-09-30 21:17:58 +0530

    todo list front end

--- app/page.js
+++ app/page.js
@@ -1,24 +1,77 @@
-"use client"
-import React, { Fragment, useState } from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+"use client";
+import React, { Fragment, useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const page = () => {
-  
-  return (
-   <Fragment>
-    <div className="container">
-      <input type="text" className = "form-control mt-5" placeholder= "Title" />
-      <textarea name="task" id="task" placeholder = "discription here ....." className='form-control mt-3 '></textarea>
-      <select className="form-control  mt-3" >
-        <option value="Due">Due</option>
-        <option value="Pending">Pending</option>
-        <option value="Completed">Completed</option>
-      </select>
-      <button type="submit" className='btn btn-primary mt-3'>Submit</button>
-    </div>
-   </Fragment>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [discription, setDiscription] = useState("");
+  const [status, setStatus] = useState("due");
 
-export default page
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newTask = {
+      title,
+      discription,
+      status,
+      date : new Date().toLocaleDateString()
+    };
+    console.log(newTask);
+  };
+
+  return (
+    <Fragment>
+      <div className="container">
+        <form onSubmit={submitHandler}>
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            type="text"
+            className="form-control mt-5"
+            placeholder="Title"
+          />
+          <textarea
+            onChange={(e) => setDiscription(e.target.value)}
+            value={discription}
+            name="task"
+            id="task"
+            placeholder="discription here ....."
+            className="form-control mt-3 "
+          ></textarea>
+          <select
+            className="form-control  mt-3"
+            onChange={(e) => setStatus(e.target.value)}
+            value={status}
+          >
+            <option value="Due">Due</option>
+            <option value="Pending">Pending</option>
+            <option value="Completed">Completed</option>
+          </select>
+          <button type="submit" className="btn btn-primary mt-3">
+            Submit
+          </button>
+        </form>
+
+        <div className="card mt-5" style={{ width: "18rem;" }}>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <h6 className="card-subtitle mb-2 text-body-secondary">
+              Card subtitle
+            </h6>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button className="btn btn-sm btn-outline-success">
+              Update Task
+            </button>
+            <button className="btn btn-sm btn-outline-danger ml-4">
+              Delete Task
+            </button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default page;
