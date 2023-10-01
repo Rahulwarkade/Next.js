b2640908c59253817c3106e4700ef872e0a66e0f
Rahul Warkade <rahulwarkade954@gmail.com>
2023-10-01 15:43:12 +0530

    TODO LIST COMPLETED

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,12 @@
 import './globals.css'
-
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <ToastContainer/>
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,22 +1,82 @@
 "use client";
 import React, { Fragment, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import {toast} from "react-toastify";
 const page = () => {
   const [title, setTitle] = useState("");
   const [discription, setDiscription] = useState("");
   const [status, setStatus] = useState("due");
-
+  const [tasks, setTasks] = useState([]);
+  const [active,setActive] = useState(null);
   const submitHandler = (e) => {
     e.preventDefault();
     const newTask = {
       title,
       discription,
       status,
-      date : new Date().toLocaleDateString()
+      date: new Date().toLocaleDateString(),
     };
-    console.log(newTask);
+
+    if(newTask.title.length<5 || newTask.discription.length<10){
+
+      toast("title or discription size is very less!!!");
+      return;
+    }
+    setTasks([...tasks, newTask]);
+
+    setTitle("");
+    setDiscription("");
+    setStatus("due");
+    setActive(null);
   };
+
+  let tasklist = (
+    <h2 className="mt-5 w-100 text-center text-danger">Loading....</h2>
+  );
+  const deleteHandler = (index) => {
+    setTasks(tasks.filter((task, idx) => idx !== index));
+  };
+
+  const updateHandler = (index) => {
+
+    const updatedTask = {
+      title : tasks[index].title,
+      discription : tasks[index].discription,
+      date : tasks[index].date,
+      status : tasks[index].status
+    };
+    deleteHandler(index);
+    setTitle(updatedTask.title);
+    setDiscription(updatedTask.discription);
+    setStatus(updatedTask.status);
+    setActive(index);
+  };
+
+
+  if (tasks.length > 0) {
+    tasklist = tasks.map((task, index) => {
+      return (
+        <div key={index} className="card mt-5" style={{ width: "18rem;" }}>
+          <div className="card-body">
+            <h5 className="card-title">{task.title}</h5>
+            <p className="card-text">{task.discription}</p>
+            <button
+              onClick={() => {updateHandler(index);}}
+              className="btn btn-sm btn-outline-success"
+            >
+              Update Task
+            </button>
+            <button
+              onClick={() => deleteHandler(index)}
+              className="btn btn-sm btn-outline-danger ml-4"
+            >
+              Delete Task
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
 
   return (
     <Fragment>
@@ -46,29 +106,14 @@ const page = () => {
             <option value="Pending">Pending</option>
             <option value="Completed">Completed</option>
           </select>
-          <button type="submit" className="btn btn-primary mt-3">
-            Submit
-          </button>
-        </form>
 
-        <div className="card mt-5" style={{ width: "18rem;" }}>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <h6 className="card-subtitle mb-2 text-body-secondary">
-              Card subtitle
-            </h6>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <button className="btn btn-sm btn-outline-success">
-              Update Task
-            </button>
-            <button className="btn btn-sm btn-outline-danger ml-4">
-              Delete Task
-            </button>
-          </div>
-        </div>
+          {active!==null ? (<button type="submit" className="btn btn-primary mt-3">
+            Update
+          </button>):(<button type="submit" className="btn btn-primary mt-3">
+            Submit
+          </button>)}
+        </form>
+        <div className="d-flex gap-4">{tasklist}</div>
       </div>
     </Fragment>
   );
