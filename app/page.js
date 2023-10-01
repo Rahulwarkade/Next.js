10681c4f41ba98e53f57acbe6df7a7c5479e16a1
Rahul Warkade <rahulwarkade954@gmail.com>
2023-10-01 19:46:46 +0530

    Components and Props

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 "use client";
 import React, { Fragment, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
+
+import Create from "@/Components/Create.js";
+import { Show } from "@/Components/Show.js";
 const page = () => {
   const [title, setTitle] = useState("");
   const [discription, setDiscription] = useState("");
   const [status, setStatus] = useState("due");
   const [tasks, setTasks] = useState([]);
-  const [active,setActive] = useState(null);
+  const [active, setActive] = useState(null);
   const submitHandler = (e) => {
     e.preventDefault();
     const newTask = {
@@ -17,8 +20,7 @@ const page = () => {
       date: new Date().toLocaleDateString(),
     };
 
-    if(newTask.title.length<5 || newTask.discription.length<10){
-
+    if (newTask.title.length < 5 || newTask.discription.length < 10) {
       toast("title or discription size is very less!!!");
       return;
     }
@@ -30,20 +32,15 @@ const page = () => {
     setActive(null);
   };
 
-  let tasklist = (
-    <h2 className="mt-5 w-100 text-center text-danger">Loading....</h2>
-  );
   const deleteHandler = (index) => {
     setTasks(tasks.filter((task, idx) => idx !== index));
   };
-
   const updateHandler = (index) => {
-
     const updatedTask = {
-      title : tasks[index].title,
-      discription : tasks[index].discription,
-      date : tasks[index].date,
-      status : tasks[index].status
+      title: tasks[index].title,
+      discription: tasks[index].discription,
+      date: tasks[index].date,
+      status: tasks[index].status,
     };
     deleteHandler(index);
     setTitle(updatedTask.title);
@@ -52,68 +49,25 @@ const page = () => {
     setActive(index);
   };
 
-
-  if (tasks.length > 0) {
-    tasklist = tasks.map((task, index) => {
-      return (
-        <div key={index} className="card mt-5" style={{ width: "18rem;" }}>
-          <div className="card-body">
-            <h5 className="card-title">{task.title}</h5>
-            <p className="card-text">{task.discription}</p>
-            <button
-              onClick={() => {updateHandler(index);}}
-              className="btn btn-sm btn-outline-success"
-            >
-              Update Task
-            </button>
-            <button
-              onClick={() => deleteHandler(index)}
-              className="btn btn-sm btn-outline-danger ml-4"
-            >
-              Delete Task
-            </button>
-          </div>
-        </div>
-      );
-    });
-  }
-
   return (
     <Fragment>
       <div className="container">
-        <form onSubmit={submitHandler}>
-          <input
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            type="text"
-            className="form-control mt-5"
-            placeholder="Title"
-          />
-          <textarea
-            onChange={(e) => setDiscription(e.target.value)}
-            value={discription}
-            name="task"
-            id="task"
-            placeholder="discription here ....."
-            className="form-control mt-3 "
-          ></textarea>
-          <select
-            className="form-control  mt-3"
-            onChange={(e) => setStatus(e.target.value)}
-            value={status}
-          >
-            <option value="Due">Due</option>
-            <option value="Pending">Pending</option>
-            <option value="Completed">Completed</option>
-          </select>
-
-          {active!==null ? (<button type="submit" className="btn btn-primary mt-3">
-            Update
-          </button>):(<button type="submit" className="btn btn-primary mt-3">
-            Submit
-          </button>)}
-        </form>
-        <div className="d-flex gap-4">{tasklist}</div>
+        <Create
+          title={title}
+          setTitle={setTitle}
+          discription={discription}
+          setDiscription={setDiscription}
+          status={status}
+          setStatus={setStatus}
+          active={active}
+          setActive={setActive}
+          submitHandler={submitHandler}
+        />
+        <Show
+          tasks={tasks}
+          updateHandler={updateHandler}
+          deleteHandler={deleteHandler}
+        />
       </div>
     </Fragment>
   );
